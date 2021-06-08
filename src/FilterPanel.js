12bdd72b31f1b9f42a305342fb9338eaeb9ce327
Josh Maas-Howard <j.maas.howard@gmail.com>
2021-06-08 15:38:05 -0400

    clean up filterPanel

--- src/FilterPanel.js
+++ src/FilterPanel.js
@@ -1,41 +1,20 @@
-import React, { useState } from "react"
+import React from "react"
 import {
-  Backdrop,
-  Box,
   ButtonGroup,
   Drawer,
   IconButton,
-  Link,
   makeStyles,
-  Modal,
   Typography,
-  useMediaQuery,
 } from "@material-ui/core"
 import _ from "lodash"
 import Select from "react-select"
-import clsx from "clsx"
-import { FilterList, Filter1Rounded, Close } from "@material-ui/icons"
+import { Close } from "@material-ui/icons"
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    // position: "fixed",
-    // width: "100%",
-    // top: "0",
-    // bottom: "0",
-    // right: "-100%",
-    // transition: "right 250ms, top 250ms",
+  paper: {
     width: "100vw",
-    // backgroundColor: "#e8f2ff",
     backgroundColor: "#e6e3d7",
     boxShadow: theme.shadows[5],
-    // "&$open": {
-    //   right: 0,
-    // },
-    // "&$tall": {
-    //   top: "10vh",
-    //   bottom: "0",
-    //   maxHeight: "500px",
-    // },
     [theme.breakpoints.up(500)]: {
       width: "254px",
     },
@@ -46,14 +25,11 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
-  open: {},
-  tall: {},
   content: {
     overflowY: "scroll",
     height: "100%",
     width: "100%",
     padding: theme.spacing(2),
-    // paddingTop: theme.spacing(4), // room for X
     boxSizing: "border-box",
   },
   header: {
@@ -87,22 +63,11 @@ const FilterPanel = ({
   togglePanelOpen,
   panelOpen,
 }) => {
-  // const [selectedOption, setSelectedOption] = useState(null);
-
-  console.log(selectedOptionsMap)
 
   const classes = useStyles()
-
-  // const isTall = useMediaQuery("(min-height:500px)")
-
-  const classNames = clsx(classes.paper, {
-    // [classes.open]: panelOpen,
-    // [classes.tall]: isTall,
-  })
   return (
     <Drawer
-      // className={classes.root}
-      classes={{ paper: [classes.root] }}
+      classes={{ paper: [classes.paper] }}
       open={panelOpen}
       variant="persistent"
       anchor="right"
